ebdad76fc44324181fa7703587dd7bc546950c93
ankit-k-maurya <31803636+ankit-k-maurya@users.noreply.github.com>
2024-10-27 17:06:44 +0530

    Update Cart.jsx

--- FRONTEND/src/Cart.jsx
+++ FRONTEND/src/Cart.jsx
@@ -4,6 +4,7 @@ import * as jose from 'jose';
 import { FaIndianRupeeSign } from "react-icons/fa6";
 import { MdDeleteForever } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import './style.css';
 
 const Cart = () => {
     const navigate = useNavigate();
