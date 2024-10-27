76e703e800c3bedabb5f004e990354c55684ca42
ankit-k-maurya <31803636+ankit-k-maurya@users.noreply.github.com>
2024-10-27 17:00:30 +0530

    Update midlwareorm.js

--- SEVERE/practicssever/midlwareorm.js
+++ SEVERE/practicssever/midlwareorm.js
@@ -1,58 +1,6 @@
 import fs from 'fs';
-import { Createschedule, getSchedule, UpdateSchedule, DeletedSchedule, Createcustomer,getEmailPassword, Createcategories, CreateProduct, GetProductDetaile, CreateOrder, CreateOrderitem, OrderDetails } from './servisesorm.js';
+import { Createcustomer,getEmailPassword, Createcategories, CreateProduct, GetProductDetaile, CreateOrder, CreateOrderitem, OrderDetails } from './servisesorm.js';
 import * as jose from 'jose';
-
-const BulidSchedule = async (req, res) => {
-    let obj;
-    const { Task, TaskDate, TaskTime } = req.body;
-    const result = await Createschedule(Task, TaskDate, TaskTime);
-    if (result) {
-        obj = { message: 'Task is successfully registerd', result }
-    }
-    else {
-        obj = { message: 'Task is not registerd' }
-    }
-    res.status(201).send(obj);
-}
-
-const getScheduleDetails = async (req, res) => {
-    let obj;
-    const result = await getSchedule();
-    if (result) {
-        obj = { message: 'Your schedule is complete today', result }
-    }
-    else {
-        obj = { message: "Your schedule is not found" }
-    }
-    res.status(200).json(obj);
-}
-
-const UpdateScheduleDetail = async (req, res) => {
-    let obj;
-    const ScheduleID = req.params.ScheduleID;
-    const { Task, TaskDate, TaskTime } = req.body;
-    const result = await UpdateSchedule(ScheduleID, Task, TaskDate, TaskTime);
-    if (result) {
-        obj = { message: 'Your Schedule is Updated', result }
-    }
-    else {
-        obj = { message: 'Your Schedule is not found' }
-    }
-    res.status(201).send(obj);
-}
-
-const deleteSchedule = async (req, res) => {
-    let obj;
-    const ScheduleID = req.params.ScheduleID;
-    const result = await DeletedSchedule(ScheduleID);
-    if (result) {
-        obj = { message: 'Your schedule is deleted', result }
-    }
-    else {
-        obj = { message: 'Your schedule is not found' }
-    }
-    res.status(200).send(obj);
-}
 
 const Bulidcustomer = async (req, res) => {
     let obj;
@@ -245,4 +193,4 @@ const sendData = async (res) =>{
        res.send(res.obj);
 }
 
-export {sendData,BulidSchedule,getScheduleDetails,UpdateScheduleDetail,deleteSchedule,BulidImage,Bulidcustomer,CustomerVerification,BulidCategory,Bulidproduct,Bulidorder,BulidOrderItem,findProductDetails,CustomerOrderDetails}
+export {sendData,BulidImage,Bulidcustomer,CustomerVerification,BulidCategory,Bulidproduct,Bulidorder,BulidOrderItem,findProductDetails,CustomerOrderDetails}
